eba63abce82a79d7c3986825f68ce35e9cfaafb0
Paul Lowndes <github@gtcode.com>
2016-01-21 15:33:42 -1000

    Progress presence mockup

--- lib/room/room_list.ex
+++ lib/room/room_list.ex
@@ -10,6 +10,7 @@ defmodule PhoenixChat.RoomList do
   end
 
   def size(room_list) do
+#    IO.inspect %{:pid => self, :as => room_list}
     Map.size(room_list.entries)
   end
 
@@ -19,10 +20,14 @@ defmodule PhoenixChat.RoomList do
   ) do
     entry = Map.put(entry, :id, auto_id)
     new_entries = Map.put(entries, entry[:user_id], entry)
-    IO.inspect new_entries
-    %PhoenixChat.RoomList{room_list |
-      entries: new_entries,
-      auto_id: auto_id + 1
+    #IO.inspect new_entries
+    #IO.inspect %{:pid => self, :as => room_list}
+    {
+      %PhoenixChat.RoomList{room_list |
+        entries: new_entries,
+        auto_id: auto_id + 1
+      },
+      entry
     }
   end
 
@@ -31,7 +36,6 @@ defmodule PhoenixChat.RoomList do
     |> Stream.filter(fn({_, entry}) ->
          entry.user_id == user_id
        end)
-
     |> Enum.map(fn({_, entry}) ->
          entry
        end)
@@ -46,9 +50,79 @@ defmodule PhoenixChat.RoomList do
     #|> Enum.map(fn({_, entry}) ->
     #     entry
     #   end)
-  end  
+  end
+  
+  def update_entries_presence(%PhoenixChat.RoomList{entries: entries, auto_id: auto_id}) do
+    #|> Stream.filter(fn({_, entry}) ->
+    #     entry.user_id == user_id
+    #   end)
+    #IO.inspect %{:before => entries}
+    new_entries = update_presence_all(entries)
+    #IO.inspect %{:after => new_entries}
+    %PhoenixChat.RoomList{entries: new_entries,
+        auto_id: auto_id
+    }
+  end
 
+  defp update_presence_all(entries) do
+    entries
+    |> Enum.map(fn {k, v} ->
+        {k, update_presence(v)}
+      end)
+    |> Enum.into(%{})
+  end
 
+  defp update_presence(entry) do
+        #  %{user_id: {user_id},
+        #name: user_id,
+        #heartbeat: user_id,
+        #presence: "present",
+        #pid: self})
+    if entry.heartbeat + 666666 < :erlang.system_time() do
+      entry = Map.put(entry, :presence, "missing")
+      #IO.inspect %{:missing => entry}
+    else
+      entry = Map.put(entry, :presence, "present")
+      #IO.inspect %{:present => entry}
+    end
+    entry
+  end
+  
+  #def update_heartbeat(%PhoenixChat.RoomList{entries: entries, auto_id: auto_id}, user_id) do
+    #entries
+    #|> Stream.filter(fn({_, entry}) ->
+   #      entry.user_id == user_id
+   #    end)
+   # |> Enum.map(fn({_, entry}) ->
+   #      #update_heartbeat_entry(entry)
+   #     entry
+  #     end)
+ #end
+ 
+  def update_heartbeat(
+    %PhoenixChat.RoomList{entries: entries} = room_list,
+    user_id
+  ) do
+    case entries[user_id] do
+      nil ->
+        IO.inspect "NIL!!!"
+        #IO.inspect entries
+        room_list
+
+      old_entry ->
+        old_entry_id = old_entry.id
+        new_entry = %{id: ^old_entry_id} = Map.put(old_entry, :heartbeat, :erlang.system_time())
+        #new_entry = %{} = updater_fun.(old_entry)
+        #new_entry = updater_fun.(old_entry)
+        new_entries = Map.put(entries, new_entry.id, new_entry)
+        %PhoenixChat.RoomList{room_list | entries: new_entries}
+    end
+  end
+
+  #defp update_heartbeat_entry(entry) do
+  #  Map.put(entry, :heartbeat, :erlang.system_time())
+  #end
+  
   def update_entry(room_list, %{} = new_entry) do
     update_entry(room_list, new_entry.id, fn(_) -> new_entry end)
   end
@@ -62,7 +136,10 @@ defmodule PhoenixChat.RoomList do
       nil -> room_list
 
       old_entry ->
-        new_entry = updater_fun.(old_entry)
+        old_entry_id = old_entry.id
+        new_entry = %{id: ^old_entry_id} = updater_fun.(old_entry)
+        #new_entry = %{} = updater_fun.(old_entry)
+        #new_entry = updater_fun.(old_entry)
         new_entries = Map.put(entries, new_entry.id, new_entry)
         %PhoenixChat.RoomList{room_list | entries: new_entries}
     end
--- lib/room/room_server.ex
+++ lib/room/room_server.ex
@@ -14,18 +14,18 @@ defmodule PhoenixChat.RoomServer do
     GenServer.start(PhoenixChat.RoomServer, nil, name: :room_server)
   end
 
-  def add_entry(room_server, new_entry) do
-    GenServer.cast(room_server, {:add_entry, new_entry})
-  end
-
-  def update_entry(room_server, {:user_id, user_id, new_entry}) do
-    GenServer.cast(room_server, {:update_entry, {:user_id, user_id, new_entry}})
-  end
+  #def update_entry(room_server, {:user_id, user_id, new_entry}) do
+  #  GenServer.cast(room_server, {:update_entry, {:user_id, user_id, new_entry}})
+  #end
 
   def entries(room_server, user_id) do
     GenServer.call(room_server, {:entries, user_id})
   end
 
+  def add_entry(room_server, new_entry) do
+    GenServer.call(room_server, {:add_entry, new_entry})
+  end
+  
   def entries(room_server) do
     GenServer.call(room_server, {:entries})
   end
@@ -34,20 +34,41 @@ defmodule PhoenixChat.RoomServer do
     GenServer.call(room_server, {:size})
   end
 
+  def update_heartbeat(room_server, user_id) do
+    GenServer.cast(room_server, {:update_heartbeat, user_id})
+  end
+
   def init(_) do
-    :timer.send_interval(2000, :heartbeat)
+    :timer.send_interval(2000, :heartbeat_loop)
     {:ok, PhoenixChat.RoomList.new}
   end
 
-  def handle_cast({:add_entry, new_entry}, room_list) do
-    new_state = PhoenixChat.RoomList.add_entry(room_list, new_entry)
-    IO.inspect new_state
+  #def handle_cast({:update_entry, {:user_id, user_id, new_entry}}, room_list) do
+  #  new_state = PhoenixChat.RoomList.update_entry()
+  #  {:noreply, new_state}
+
+  #end
+  def handle_cast({:update_heartbeat, user_id}, room_list) do
+    new_state = PhoenixChat.RoomList.update_heartbeat(room_list, user_id)
+    IO.inspect [:newbeat,  new_state, :user_id, user_id, :room_list,  room_list]
+    #{:noreply, new_state}
     {:noreply, new_state}
   end
 
-  def handle_cast({:update_entry, {:user_id, user_id, new_entry}}, room_list) do
-    new_state = PhoenixChat.RoomList.update_entry()
-    {:noreply, new_state}
+
+  def handle_call({:add_entry, new_entry}, _, room_list) do
+    #IO.inspect %{:rlll => room_list}
+    {new_state, updated_new_entry} = PhoenixChat.RoomList.add_entry(room_list, new_entry)
+
+    #new_state = Todo.List.add_entry(todo_list, new_entry)
+    #Todo.Database.store(name, new_state)
+    #IO.puts "new state:"
+    #IO.inspect %{:n => new_state}
+    {
+      :reply,
+      updated_new_entry,
+      new_state
+    }
   end
 
   def handle_call({:entries, user_id}, _, room_list) do
@@ -74,9 +95,37 @@ defmodule PhoenixChat.RoomServer do
     }
   end
   
-  def handle_info(:heartbeat, state) do
+  def handle_info(:heartbeat_loop, room_list) do
+    
+  
+    #room_server = PhoenixChat.RoomServer.start_single
+    #user = PhoenixChat.RoomServer.add_entry(room_server,
+    #  %{user_id: {user_id},
+    #    name: user_id,
+    #    heartbeat: user_id,
+    #    presence: "present",
+    #    pid: self})
+    #x = PhoenixChat.RoomServer.size(room_server)
+    #state = PhoenixChat.RoomServer.entries(room_server)
+    #IO.inspect %{:heartbeat_state => room_list}
+    #id = user.user_id
+    
+    #new_presence = "missing"  # "missing"
+
+    #   IO.inspect %{:heartbeat_state => state}
+    new_state = PhoenixChat.RoomList.update_entries_presence(room_list)
+    #IO.inspect %{:map_to_list_heartbeat_handle_info_state => Map.to_list(state)}
+    #IO.inspect %{:map_to_list_heartbeat_handle_info_state => state}
+    #PhoenixChat.RoomList.update_entry(state, id, &Map.put(id, :presence, new_presence))
+
+#    new_state = state
+    #token = Phoenix.Token.sign(conn, "user", id)
+    #assign(conn, :user_token, token)
+  
+  
     PhoenixChat.Endpoint.broadcast! "rooms:lobby", "heartbeat", %{:time => :erlang.system_time(), :user_list => "user_list"}
-    {:noreply, state}
+    #IO.inspect %{:nesta => new_state}
+    {:noreply, new_state}
   end
   
   # Needed for testing purposes
